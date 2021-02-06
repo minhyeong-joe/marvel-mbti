716eba5f7b063927f70fc251519d4e2d985efdad
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2021-02-05 22:10:05 -0800

    finished assessmet UI

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -1,12 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { LanguageContext } from "../LanguageContext";
 import { questions } from "../questions";
 
 const Assessment = () => {
 	const [lang, setLang] = useContext(LanguageContext);
+	const [currentQ, setCurrentQ] = useState(0);
+	const [scores, setScores] = useState({ IvsE: 0, SvsN: 0, TvsF: 0, JvsP: 0 });
 	console.log(questions);
-	return <div>Assessment</div>;
+	return (
+		<>
+			<p className="question-counter">
+				{currentQ + 1} / {questions.length}
+			</p>
+			<div className="question-container">
+				<h1 className="number">Q.{currentQ + 1}</h1>
+				<h2 className="prompt">{questions[currentQ].prompt[lang]}</h2>
+				<div className="options-container">
+					<button className="option-btn btn">
+						{questions[currentQ].optionA[lang]}
+					</button>
+					<button className="option-btn btn">
+						{questions[currentQ].optionB[lang]}
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Assessment;
