89947bd90f0cdcf102b90661125e5e60dca9d80f
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2021-02-05 16:35:36 -0800

    added results template

---
+++ src/results.js
@@ -0,0 +1,274 @@
+export const results = {
+	istj: {
+		adj: {
+			kr: "한번 맡은 임무는 끝까지 완수하는",
+		},
+		job: {
+			kr: "쉴드 요원",
+		},
+		name: {
+			kr: "마리아 힐",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	isfj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "캡틴 아메리카",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	infj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "비전",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	intj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "닥터 스트레인지",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	istp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "블랙 위도우",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	isfp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "블랙 팬서",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	infp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "스파이더맨",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	intp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "브루스 배너",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	estp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "스타로드",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	esfp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "드랙스",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	enfp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "토르",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	entp: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "아이언맨",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	estj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "타노스",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	esfj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "워머신",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	enfj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "앤트맨",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+	entj: {
+		adj: {
+			kr: "",
+		},
+		job: {
+			kr: "",
+		},
+		name: {
+			kr: "닉 퓨리",
+		},
+		image_url: "",
+		description: {
+			kr: "상세설명",
+		},
+		good_relation: [],
+		bad_relation: [],
+	},
+};
