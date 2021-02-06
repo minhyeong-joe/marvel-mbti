801775e3fe681a95dcdd21c671759597f2abe944
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2021-02-05 22:39:46 -0800

    implemented assessment and result calculation

--- src/components/Assessment.js
+++ src/components/Assessment.js
@@ -3,11 +3,30 @@ import React, { useContext, useState } from "react";
 import { LanguageContext } from "../LanguageContext";
 import { questions } from "../questions";
 
-const Assessment = () => {
+const Assessment = ({ history }) => {
 	const [lang, setLang] = useContext(LanguageContext);
 	const [currentQ, setCurrentQ] = useState(0);
 	const [scores, setScores] = useState({ IvsE: 0, SvsN: 0, TvsF: 0, JvsP: 0 });
-	console.log(questions);
+
+	const onClickOption = (qtype, score) => {
+		setScores({ ...scores, [qtype]: scores[qtype] + score });
+		if (currentQ < questions.length - 1) {
+			setCurrentQ(currentQ + 1);
+		} else {
+			const result = computeResult();
+			history.push(`/result/${result}`);
+		}
+	};
+
+	const computeResult = () => {
+		let result = "";
+		result += scores.IvsE <= 0 ? "i" : "e";
+		result += scores.SvsN <= 0 ? "s" : "n";
+		result += scores.TvsF <= 0 ? "t" : "f";
+		result += scores.JvsP <= 0 ? "j" : "p";
+		return result;
+	};
+	console.log(scores);
 	return (
 		<>
 			<p className="question-counter">
@@ -17,10 +36,26 @@ const Assessment = () => {
 				<h1 className="number">Q.{currentQ + 1}</h1>
 				<h2 className="prompt">{questions[currentQ].prompt[lang]}</h2>
 				<div className="options-container">
-					<button className="option-btn btn">
+					<button
+						className="option-btn btn"
+						onClick={() =>
+							onClickOption(
+								questions[currentQ].type,
+								questions[currentQ].optionA.score
+							)
+						}
+					>
 						{questions[currentQ].optionA[lang]}
 					</button>
-					<button className="option-btn btn">
+					<button
+						className="option-btn btn"
+						onClick={() =>
+							onClickOption(
+								questions[currentQ].type,
+								questions[currentQ].optionB.score
+							)
+						}
+					>
 						{questions[currentQ].optionB[lang]}
 					</button>
 				</div>
