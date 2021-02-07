da90373cd78d7d6b68fb9094430e601e13029508
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2021-02-06 16:54:30 -0800

    changed share button on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { LanguageContext } from "../LanguageContext";
+import { FaFacebookF, FaCopy } from "react-icons/fa";
+import FlashMessage from "react-flash-message";
 import Constants from "../language";
 import images from "../assets";
 
 const Landing = () => {
 	const [lang, setLang] = useContext(LanguageContext);
+	const [showFlashMessage, setShowFlashMessage] = useState(false);
 	console.log(lang);
+
+	const onClickCopyLink = (link) => {
+		setShowFlashMessage(false);
+
+		navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+			if (result.state === "granted" || result.state === "prompt") {
+				navigator.clipboard.writeText(link).then(
+					() => {
+						setShowFlashMessage(true);
+						setTimeout(() => {
+							setShowFlashMessage(false);
+						}, 1500);
+					},
+					() => {
+						console.log("failed to copy to clipboard");
+					}
+				);
+			}
+		});
+	};
+
 	return (
 		<div className="landing">
 			<p
@@ -21,7 +45,30 @@ const Landing = () => {
 				<Link to="/assessment" className="btn btn-primary start-btn">
 					{Constants.start[lang]}
 				</Link>
-				<button className="btn btn-secondary">{Constants.share[lang]}</button>
+				<div className="share-container">
+					<h3 className="header">{Constants.shareApp[lang]}</h3>
+					<div className="icon-btn-container">
+						<button
+							className="btn btn-icon"
+							onClick={() => onClickCopyLink(window.location.origin)}
+						>
+							<FaCopy color="black" />
+						</button>
+						<button className="btn btn-icon katalk">
+							<img src={images.katalk.default} alt="katalk" />
+						</button>
+						<button className="btn btn-icon facebook">
+							<FaFacebookF />
+						</button>
+					</div>
+				</div>
+				{showFlashMessage && (
+					<FlashMessage duration={1500}>
+						<div className="flash-message">
+							<p>Link copied to Clipboard!</p>
+						</div>
+					</FlashMessage>
+				)}
 				<img
 					src={images.captain_america.default}
 					alt="captain_america"
