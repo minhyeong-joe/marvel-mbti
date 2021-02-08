cd397c153f818329e0276e4378ce580edb5ea610
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2021-02-07 18:10:04 -0800

    reverted dynamic og

--- src/components/Share.js
+++ src/components/Share.js
@@ -39,28 +39,11 @@ const Share = ({ header, title, description, imageUrl, sharedUrl }) => {
 	};
 
 	const onClickFacebook = () => {
-		const head = document.querySelector("head");
-		const original = head.innerHTML;
-		const newMetaHtml = `
-		<meta property="og:type" content="website">
-		<meta property="og:url" content="${sharedUrl}">
-		<meta property="og:title" content="${title}">
-		<meta property="og:description" content="${description}">
-		<meta property="og:image" content="${imageUrl}">
-	
-		<!-- Twitter -->
-		<meta property="twitter:card" content="summary_large_image">
-		<meta property="twitter:url" content="${sharedUrl}">
-		<meta property="twitter:title" content="${title}">
-		<meta property="twitter:description" content="${description}">
-		<meta property="twitter:image" content="${imageUrl}">`;
-		head.innerHTML += newMetaHtml;
 		window.FB.ui({
 			display: "popup",
 			method: "share",
 			href: sharedUrl,
 		});
-		head.innerHTML = original;
 	};
 
 	return (
